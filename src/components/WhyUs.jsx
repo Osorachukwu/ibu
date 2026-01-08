e2c9e9ba7c1ab1bb26b92d6551a44d9817d70a99
Osorachukwu <oezewi@gmail.com>
2026-01-08 22:25:58 +0100

    small update

--- src/components/WhyUs.jsx
+++ src/components/WhyUs.jsx
@@ -6,7 +6,7 @@ import roadShipment from "../assets/truck-on-the-road.jpg"
 export default function WhyUs() {
     return (
         <div className='px-3 lg:px-16'>
-            <div className='card bg-base-300 container px-4 mx-auto py-5 md:p-10 mb-10 rounded-xl md:rounded-3xl'>
+            <div className='card bg-base-300 container px-2 mx-auto py-5 md:p-10 mb-10 rounded-xl md:rounded-3xl'>
                 <div className='max-w-4xl mb-6'>
                     <p className='text-4xl md:text-5xl font-bold mb-5'>Why choose us.</p>
                     <p className='sm:text-xl'>Because Terek will make it easier to send your package with the features we have.</p>
@@ -40,14 +40,14 @@ export default function WhyUs() {
 
                 <hr className='my-16 text-gray-400' />
 
-                <div className='grid grid-cols-2 md:grid-cols-3 gap-2 justify-items-center'>
+                <div className='grid grid-cols-2 md:grid-cols-3 sm:gap-2 justify-items-center'>
                     {[
                         { title: "Real time tracking", subTitle: "You can check the status of the shipment in real time using your shipment code." },
                         { title: "Pickup Delivery", subTitle: "To add to the convenience & security of your shipmeny, we provide a pick-up service that can make it easier for you" },
                         { title: "Insurance Service", subTitle: "Provides protection for your package which adds security when shipping goods" }, { title: "24 Hours", subTitle: "Generate something with ai for this" }, { title: "Packaging", subTitle: "We provide packaging services. Let us know if you need this service, we will be happy to help you" }, 6].map((item, i) => (
                             <div key={i} className="card w-full card-sm sm:card-md lg:card-xl shadow-sm bg-base-100 border border-base-300 transition-all ease-in-out duration-100 hover:border-secondary/30 rounded-3xl">
                                 <div className="card-body">
-                                    <h1 className='text-4xl font-bold'>0{i + 1}</h1>
+                                    <h1 className='text-xl sm:text-4xl font-bold'>0{i + 1}</h1>
                                     <h2 className="card-title ">{item.title}</h2>
                                     <p>{item.subTitle}</p>
                                 </div>
